838da26c197cbfa64942ad1bff47c8e494fa76bf
SPISHIO <88729599+SPISHIO@users.noreply.github.com>
2022-02-19 09:01:07 +0200

    Add files via upload

---
+++ Contact.js
@@ -0,0 +1,7 @@
+document.getElementById("c0").innerHTML="Contact us";
+document.getElementById("c1").innerHTML="Welcome to My website, your one spot for all your IT solutions";
+document.getElementById("c2").innerHTML="Our contact details are as follows:";
+document.getElementById("c4").innerHTML="Cell number: 0837577444";
+document.getElementById("c5").innerHTML="sibusisosigudla21@gmail.com";
+document.getElementById("c6").innerHTML="";
+document.getElementById("c7").innerHTML="Pretoria, Tshwane, South Africa.";
---
+++ index.js
@@ -0,0 +1,7 @@
+        
+        /* home*/
+        document.getElementById("HEADING").innerHTML="Sibusiso Sigudla";
+			document.getElementById("headingcont").innerHTML=" I have a strong interest on technology; innovation, artificial intelligance, information security and development, using different languages; c#, Javascript, Java, R and etc. Above is the demonstration video of the system i developed using R.";
+			document.getElementById("headingcont2").innerHTML="I am able to work under stressful conditions. I am reliable, time conscious,responsible and always look forward to solving problems. I am in possesion of a Bachelors degree in information systems, passointe about software development.";
+			document.getElementById("headingcont2").innerHTML="I am a freelancer developer and a participant in Codetribe academy's 2022 bootcamp, seeking to polish upm my skill and gain more knowladge about the industry that i love so much. My future plans are to start my own software development company so that i can help graduates gain entrency into the industry. Above attached is the vidoe showcasing the machine learning system i developed using Rstudio";
+	
